fa0decfbbbd66ec0ccf6a41e1b018fb33b78d3b9
Eimer Dev <eimeraibaa@gmail.com>
2025-07-08 23:23:59 -0500

    deploying first back end api

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,23 @@
 import Sequelize from 'sequelize';
 
-export const sequelize = new Sequelize("postgres", "postgres", "manager", {
-  host: "localhost",
-  dialect: "postgres"
-});
+const isProd = process.env.NODE_ENV === 'production';
+
+export const sequelize = isProd ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  // En desarrollo seguimos usando host, user y pass locales
+  : new Sequelize('postgres', 'postgres', 'manager', {
+      host: 'localhost',
+      dialect: 'postgres',
+      logging: console.log,
+    });
+
+export default sequelize;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
 
+const port = process.env.PORT || 3000;
+
 async function main() {
   try {
     await sequelize.sync({ force: true }); // Use force: true to drop and recreate tables
-    app.listen(3000);
+
+    app.listen(port, () => {
+      console.log("API escuchando en el puerto", port);
+    });
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
