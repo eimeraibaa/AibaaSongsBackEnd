2f1367fc6cff6c96ba9531853a7a48c204558516
Eimer Dev <eimeraibaa@gmail.com>
2025-11-04 22:42:40 -0500

    cambio a Session_Secret

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const sessionStore = new SequelizeStore({
 });
 
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'tu-clave-secreta-super-segura',
+  secret: process.env.SESSION_SECRET,
   store: sessionStore,
   resave: false,
   saveUninitialized: false,
